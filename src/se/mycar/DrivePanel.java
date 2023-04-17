4fc8341a140f4f0137c3ef515aee60e00e1092f0
Nickname <1013238728@qq.com>
2023-04-17 23:31:44 +0800

    SE

---
+++ src/se/mycar/DrivePanel.java
@@ -0,0 +1,12 @@
+package se.mycar;
+
+public class DrivePanel {
+    private DataComputer dataComputer;
+    private double mpg;
+    private double mph;
+    private double distance;
+
+    DrivePanel(DataComputer dataComputer){
+        this.dataComputer = dataComputer;
+    }
+}
