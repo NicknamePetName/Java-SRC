dcc85e19163fdbbb5d98bbd86b44673c4a9da45c
Nickname <1013238728@qq.com>
2023-04-20 18:36:30 +0800

    SE

--- src/se/mycar/DisplayMphImpl.java
+++ src/se/mycar/DisplayMphImpl.java
@@ -8,9 +8,16 @@ public class DisplayMphImpl implements IDisplayInterface {
     @Override
     public void getDisplay() {
         System.out.println(dataComputer.getMph()+"Km/H");
-        if (dataComputer.getMph() > 55) {
-            System.out.println("发出警报，您已超速！");
-        }
+        if (dataComputer.getMph() > 30) {
+            System.out.println("加速");
+            if (dataComputer.getMph() > 55) {
+                System.out.println("发出警报，您已超速！");
+            }
+        }else if (dataComputer.getMph() <30) {
+            System.out.println("减速");
+        }else {
+            System.out.println("匀速行驶");
+        }  
     }
     
 }
